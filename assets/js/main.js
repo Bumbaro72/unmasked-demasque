dba85d3062caf67e9c2cf7210b3380df852ed3b8
Brux-Stream <brux.stream@gmail.com>
2025-04-19 11:10:49 +0200

    “Something”

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,20 @@
 // Animacija kasnije
 
-const observer = new IntersectionObserver(
-    (entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('start');
-          observer.unobserve(entry.target); // animira se samo jednom
-        }
-      });
-    },
-    {
-      threshold: 0.5 // najmanje 50% elementa mora ući u viewport
-    }
-  );
 
-  
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('start');
+        observer.unobserve(entry.target); // animira se samo jednom
+      }
+    });
+  },
+  {
+    threshold: 0.5 // najmanje 50% elementa mora ući u viewport
+  }
+);
+
+document.querySelectorAll('.animacija-na-scroll').forEach(elem => {
+  observer.observe(elem);
+});
