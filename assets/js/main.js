a3f8f7d27a98b276a28224c2ce895ab1e95c822b
Brux-Stream <brux.stream@gmail.com>
2025-04-19 17:59:31 +0200

    “Something”

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 // Animacija kasnije
 
-
 document.addEventListener("DOMContentLoaded", function () {
   function onScrollAnimation() {
     document.querySelectorAll(".animacija-na-scroll").forEach(el => {
@@ -23,4 +22,30 @@ document.addEventListener("DOMContentLoaded", function () {
   onScrollAnimation(); // pokreni odmah
 });
 
+// FADE in/out
+  const fadeOverlay = document.getElementById('fade-overlay');
+
+  // FADE IN
+  window.addEventListener('load', () => {
+    fadeOverlay.classList.add('fade-out');
+  });
+
+  // FADE OUT PRI KLIKOVIMA NA LINKOVE
+  document.querySelectorAll('a').forEach(link => {
+    if (link.getAttribute('target') === '_blank') return; // ignoriraj nove tabove
+
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const href = this.getAttribute('href');
+
+      fadeOverlay.classList.remove('fade-out'); // vrati na crno
+      fadeOverlay.style.opacity = 1;
+
+      setTimeout(() => {
+        window.location.href = href;
+      }, 300); // trajanje fade-out animacije
+    });
+  });
+
+  
 
