b1340029320e387a57c1bbbac9ee5eb468fa7493
Brux-Stream <brux.stream@gmail.com>
2025-04-20 03:49:14 +0200

    “Stvarno”

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 // Animacija kasnije
 
+
 document.addEventListener("DOMContentLoaded", function () {
   function onScrollAnimation() {
     document.querySelectorAll(".animacija-na-scroll").forEach(el => {
@@ -22,39 +23,4 @@ document.addEventListener("DOMContentLoaded", function () {
   onScrollAnimation(); // pokreni odmah
 });
 
-// FADE in/out
- 
-  document.addEventListener("DOMContentLoaded", () => {
-    const fadeOverlay = document.getElementById('fade-overlay');
 
-    // FADE IN: pokreni kad se DOM učita
-    window.requestAnimationFrame(() => {
-      fadeOverlay.classList.add('fade-out');
-    });
-
-    // FADE OUT na klik linka
-    document.querySelectorAll('a').forEach(link => {
-      const href = link.getAttribute('href');
-
-      // Ignoriraj eksterne linkove i #anchor
-      if (
-        !href ||
-        href.startsWith('#') ||
-        link.target === '_blank' ||
-        href.startsWith('http')
-      ) return;
-
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        fadeOverlay.classList.remove('fade-out');
-        fadeOverlay.style.opacity = 1;
-
-        setTimeout(() => {
-          window.location.href = href;
-        }, 100);
-      });
-    });
-  });
-
-// Hmburger toggle
